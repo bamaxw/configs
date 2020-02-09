3dcd5e01a7b340264725f0a43eeb24407190485b
Max Wasylow <bamwasylow@gmail.com>
2020-02-09 18:52:59 +0000

    starting-over

---
+++ zsh/config.sh
@@ -0,0 +1,11 @@
+## Inspired by:
+## https://www.freecodecamp.org/news/how-to-configure-your-macos-terminal-with-zsh-like-a-pro-c0ab3f3c1156/
+
+ZSH_THEME="powerlevel9k/powerlevel9k"
+
+plugins=(
+	git
+	docker
+	zsh-syntax-highlighting
+	zsh-autosuggestion
+)
